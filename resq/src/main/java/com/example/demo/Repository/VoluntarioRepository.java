d2df1d80babf3950ed8b04756a4b0f7c9af5f5f6
Diego Ramírez <diego.ramirez.vi@usach.cl>
2023-07-14 15:08:58 -0400

    fix: Error de nombre

--- resq/src/main/java/com/example/demo/Repository/VoluntarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Models.Voluntario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface VoluntarioRepository extends MongoRepository<Voluntario, String> {
-
-}
